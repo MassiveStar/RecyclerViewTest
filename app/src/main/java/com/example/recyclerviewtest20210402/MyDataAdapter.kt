60ffae968529fba171ea12ed0b20dd9a7e33ede6
Elvis Franks Condori Arias <massiveStar7@gmail.com>
2021-04-02 11:21:35 -0500

    Create class MyDataAdapter

---
+++ app/src/main/java/com/example/recyclerviewtest20210402/MyDataAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.recyclerviewtest20210402
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyDataAdapter : RecyclerView.Adapter<MyDataAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyDataAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyDataAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+}
