72e9457cdf1c139d9b7247cbeba964870345fc24
Elvis Franks Condori Arias <massiveStar7@gmail.com>
2021-04-02 11:08:38 -0500

    Implementacion list_item_...

--- app/src/main/java/com/example/recyclerviewtest20210402/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtest20210402/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list_data: List<MyData> = listOf(
+            MyData(2, "Juan", "12345"),
+            MyData(2, "Franks", "56789"),
+            MyData(3, "Edison", "34567")
+        )
+
+
     }
 }
