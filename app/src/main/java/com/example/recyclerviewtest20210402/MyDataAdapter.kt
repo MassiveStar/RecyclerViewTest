4f9ba6f2d5ed8e4f8bb39f4ad41c7ce99ae66941
Elvis Franks Condori Arias <massiveStar7@gmail.com>
2021-04-04 11:37:15 -0500

    Use dataBinding in ViewHolder

--- app/src/main/java/com/example/recyclerviewtest20210402/MyDataAdapter.kt
+++ app/src/main/java/com/example/recyclerviewtest20210402/MyDataAdapter.kt
@@ -1,13 +1,11 @@
 package com.example.recyclerviewtest20210402
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewtest20210402.databinding.ListItemMainActivityBinding
 
 class MyDataAdapter : ListAdapter<MyData, MyDataAdapter.ViewHolder>(MyDataDiffCallback()) {
 
@@ -20,15 +18,12 @@ class MyDataAdapter : ListAdapter<MyData, MyDataAdapter.ViewHolder>(MyDataDiffCa
         holder.bind(item)
     }
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val nameText: TextView = itemView.findViewById(R.id.name_text)
-        val dniText: TextView = itemView.findViewById(R.id.dni_text)
-        val sampleImage: ImageView = itemView.findViewById(R.id.imageView)
+    class ViewHolder private constructor(val binding: ListItemMainActivityBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: MyData) {
-            nameText.text = item.name
-            dniText.text = item.dni
-            sampleImage.setImageResource(
+            binding.nameText.text = item.name
+            binding.dniText.text = item.dni
+            binding.imageView.setImageResource(
                 when (item.image) {
                     1 -> R.drawable.andrea_1
                     2 -> R.drawable.betsy_2
@@ -40,10 +35,10 @@ class MyDataAdapter : ListAdapter<MyData, MyDataAdapter.ViewHolder>(MyDataDiffCa
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater
-                    .inflate(R.layout.list_item_main_activity, parent, false)
+                val binding =
+                    ListItemMainActivityBinding.inflate(layoutInflater,parent,false)
 
-                return ViewHolder(view)
+                return ViewHolder(binding)
             }
         }
     }
